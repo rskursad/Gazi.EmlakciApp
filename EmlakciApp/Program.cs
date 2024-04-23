e2ede9fdc93accf8982bbbe0f4967e36e041464b
Kürşad's PC <Krad's PC@DESKTOP-URFB7VL>
2024-04-23 17:12:53 +0300

    Add project files.

---
+++ EmlakciApp/Program.cs
@@ -0,0 +1,123 @@
+﻿using EmlakciLib;
+using System.Threading.Channels;
+
+namespace EmlakciApp
+{
+    internal class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Ev evim; // ev tipinde evim isminde referans
+            evim = new Ev();// ev tipinde yeni bir nesne
+            //evim.alan = 120;
+            //evim.odasayisi = 3;
+            //evim.katno = 1;
+            //evim.semt = "Gazi";
+            //Console.WriteLine(EvBilgi(evim));
+
+            //Console.WriteLine();
+
+            //Ev evim2 = new Ev();
+            //evim2.alan = 100;
+            //evim2.katno = 2;
+            //evim2.odasayisi = 3;
+            //evim2.semt = "Kazan";
+            //Console.WriteLine(EvBilgi(evim2));
+
+            //Console.WriteLine();
+
+            //try
+            //{
+            //    Ev evim3=null;
+            //    Console.WriteLine(EvBilgi(evim3));
+            //}
+            //catch (NullReferenceException)
+            //{
+            //    Console.WriteLine("Referans null geldi.");
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("Bir hata oluştu.");
+            //}
+
+            //Console.WriteLine("Oda sayısı giriniz:");
+            //evim.SetOdaSayisi(int.Parse(Console.ReadLine()));
+            //Console.WriteLine("kat no giriniz:");
+            //evim.Katno = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Semt giriniz:");
+            //evim.Semt=Console.ReadLine();
+            //Console.WriteLine("alan giriniz:");
+            //evim.alan=double.Parse(Console.ReadLine());
+
+            //Console.WriteLine($"Girilen ev bilgileri:\n{evim.EvBilgi(evim)}");
+            //Console.WriteLine("Oda sayısı:"+evim.GetOdaSayisi);
+            //Console.WriteLine("Semt:" + evim.Semt);
+
+            //int sayi = 5; //set
+            //Console.WriteLine(sayi); //get
+
+            //double alan;
+            //Console.WriteLine("Evin alanını giriniz:");
+            //alan = double.Parse(Console.ReadLine());
+            //var evim= new Ev(3,2,"Gazi",alan);
+            //Console.WriteLine(evim.Alan);
+
+
+            Ev evim2 = new Ev(3,2,100);
+            Console.WriteLine(evim2.EvBilgi(evim2));
+
+            Console.WriteLine("Bellekteki ev sayısı: "+Ev.Count);
+
+
+
+        }
+        //static string EvBilgi(Ev ev)
+        //{
+        //    //if (ev != null)
+        //    //{
+        //    //    return "Oda sayısı:" + ev.odasayisi + "\nKat no:" + ev.katno + "\nAlan:" + ev.alan + "\nSemt:" + ev.semt;
+        //    //}
+        //    //else
+        //    //{
+        //    //    return "Referans null geldi!";
+        //    //}
+        //    try
+        //    {
+        //        return "Oda sayısı:" + ev.odasayisi + "\nKat no:" + ev.katno + "\nAlan:" + ev.alan + "\nSemt:" + ev.semt;
+        //    }
+        //    catch (NullReferenceException ex)
+        //    {
+        //        throw ex;
+        //    }
+        //    catch (Exception)
+        //    {
+        //        throw;
+        //    }
+
+        //}
+
+
+    }
+
+    //class Ev
+    //{
+    //    public int odasayisi;  //field
+    //    public int katno;
+    //    public double alan;
+    //    public string semt;
+
+    //}
+}
+//Kapsülleme ilkesi (Encapsulation):Bir class içerisinde yapılan işlemlerin, diğer classlardan gizlenmesi ilkesidir.
+//Property: Private yapılara ulaşmak için kullanılan yapılardır. Get ve  opsiyonel olarak set yapıları içerir.
+
+// Referanslar belleğin stack bölgesinde tutulur.
+// Referanslar, heap bölgesindeki nesnenin adresini tutar.
+//Nesneler belleğin heap bölgesinde tutulur.
+// Nesne oluşturulurken kullanılan new anahtar sözcüğü, heap bölgesinde alan ayırma işlemi yaparken nesne içerisinde bulunan fieldlara
+//varsayılan değerlerin de atamasını yapar.* (Constructor method)
+// new; hesap bölgesinde tutulan yeni bir nesne oluşturmaya yarar.
+//Bir class oluşturduğumuzda aslında "int" gibi yeni bir tür oluşturuyoruz. Bu türü daha sonra "int" gibi kullanabiliyoruz.
+//Metodlarda tanımladığımız class ları kullanabilmek için class türünde parametre veriyoruz.
---
+++ EmlakciLib/Ev.cs
@@ -0,0 +1,126 @@
+﻿namespace EmlakciLib
+{
+    public class Ev
+    {
+        private static int count=0;
+        public static int Count { get => count;  }
+        public Ev(int odasayisi, int katno,string semt, double alan) // geri dönüş tipi yok
+        {
+            this.Odasayisi = odasayisi;
+            this.Katno = katno;
+            this.Semt = semt;
+            this.Alan = alan;
+            count++;
+        }
+        public Ev()
+        {
+            count++;
+        }
+        public Ev(int odasayisi, int katno,  double alan) // geri dönüş tipi yok
+        {
+            this.Odasayisi = odasayisi;
+            this.Katno = katno;
+            this.Alan = alan;
+            this.Semt = "Gazi";
+            count++;
+        }
+
+
+        private int odasayisi;
+        private int katno;
+        //private double alan;
+        private string semt;
+        private string buyukluk;
+
+
+        
+        public int Katno { get => katno; set => katno = value; }//property 
+        public string Semt { get => semt; set => semt = value.ToUpper(); }
+        //public double Alan { get => alan; set => alan = Math.Abs(value); }
+
+        public int Odasayisi { get => odasayisi; set => odasayisi = Math.Abs(value); }
+
+        //propfull tab tab
+        private double alan;
+
+        public double Alan
+        {
+            get 
+            {
+                string buyukluk;
+                buyukluk= this.buyukluk;
+                return alan;
+            }
+
+            set
+            {
+                alan = alan;
+                if (alan < 100)
+                {
+                    this.buyukluk = "Küçük Ev";
+                }
+                else if (100 <= alan && alan < 150)
+                {
+                    this.buyukluk = "Orta Ev";
+                }
+                else
+                {
+                    this.buyukluk = "Büyük Ev";
+                }
+            }
+        }
+
+
+
+
+        //public void SetAlan(double alan)
+        //{
+        //    this.alan = alan;
+        //    if (alan < 100)
+        //    {
+        //        this.buyukluk = "Küçük Ev";
+        //    }
+        //    else if (100 <= alan && alan < 150)
+        //    {
+        //        this.buyukluk = "Orta Ev";
+        //    }
+        //    else
+        //    {
+        //        this.buyukluk = "Büyük Ev";
+        //    }
+        //}
+        public string Buyukluk { get => buyukluk; }
+       
+
+
+        //public void SetSemt(string semt)
+        //{
+        //    this.semt = semt.ToUpper();
+        //}
+
+        //public string GetSemt()
+        //{
+        //    return this.semt;
+        //}
+
+
+        public void SetOdaSayisi(int odasayisi)
+        {
+            this.Odasayisi = Math.Abs(odasayisi);
+        }
+
+        public int GetOdaSayisi(int odasayisi)
+        {
+            return this.Odasayisi;
+        }
+
+
+        public string EvBilgi(Ev ev)
+        {
+                return "Oda sayısı:" + this.Odasayisi + "\nKat no:" + this.Katno + "\nAlan:" + this.Alan + "\nSemt:" + this.Semt;
+           
+        }
+    }
+}
+//Default CONSTRUCTOR : Class içerisinde bulunan fieldlara varsaılan değerleri atayarak kullanıma hazır hale getirir.
+// Constructor ların geri dönüş tipi olmaz. Class ile aynıdır.
